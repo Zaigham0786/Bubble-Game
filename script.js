a27cd4f860aeb1c2b74867f2b286c06bf7ac6989
Zaigham0786 <mdzaigham26@gmail.com>
2023-08-28 13:13:05 +0530

    completed

---
+++ script.js
@@ -0,0 +1,66 @@
+const makeBubble = () => {
+    var bubble = ""
+    for (var i = 1; i <= 70; i++) {
+        var res = Math.floor(Math.random() * 10)
+        bubble += `<div class="bubble" id="bubbleval">${res}</div>`
+    }
+    document.querySelector(".bottom").innerHTML = bubble
+}
+// crating audio 
+var Success = function (audioName) {
+    let audio = new Audio(audioName)
+    audio.play();
+}
+var audio = function () {
+    Success("success.mp3")
+}
+var erroraudio = function () {
+    Success("error.mp3")
+}
+/// above sectiom is for au
+var timer = 0
+const runTimer = () => {
+    var timerval = setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timer").textContent = timer;
+        }
+        else {
+            clearInterval(timerval)
+            document.querySelector(".bottommain").innerHTML = `
+            <div id="result">
+              <p>Game Over</p>
+              <p>Your Score : <span>${score}</span></p>
+              
+            </div>
+
+            `
+        }
+    }, 1000);
+}
+var hitrn = 0
+const Hit = () => {
+    hitrn = Math.floor(Math.random() * 10)
+    document.getElementById("hitval").textContent = hitrn
+}
+
+Hit()
+runTimer()
+makeBubble()
+var score = 0
+const increaseScore = () => {
+    score += 5;
+    document.querySelector("#scoreval").textContent = score
+}
+document.querySelector(".bottom").addEventListener("click", function (data) {
+    var clickedNum = (Number(data.target.textContent))
+    if (clickedNum === hitrn) {
+        increaseScore()
+        makeBubble()
+        Hit()
+        audio()
+    }
+    else {
+        erroraudio()
+    }
+})
